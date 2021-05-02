607941f9b8b65405bd18d86ec53ab374daabff95
SubhadityaMukherjee <msubhaditya@gmail.com>
2021-05-02 23:07:22 +0530

    added_post_creation_and_basic_backbone

--- pusher.sh
+++ pusher.sh
@@ -1,8 +1,7 @@
 black "."
-python runner.py -f "demo.txt" -o "syntax.pdf" -c True -n "Subhaditya Mukherjee" -t "pyGroff" -w True
 pdoc --force --html -o docs pygroff
-mv docs/pygroff/index.html docs/index.md
-mv docs/pygroff/* docs/
+mv docs/selket/index.html docs/index.md
+mv docs/selket/* docs/
 if [[ ! -z $1 ]]; then
         git add . && git commit -m $1 && git push
 fi
